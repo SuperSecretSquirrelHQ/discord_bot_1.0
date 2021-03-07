452d84889640196a0be4bc5ea00d9a8f35aac683
Colenk <>
2021-03-06 19:00:46 -0500

    Redid comments and added some logging, no functionality yet
    Co-authored-by: Mrlilal <Mrlilal@users.noreply.github.com>

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,9 +1,8 @@
-//here the event start
 module.exports = async (client, reaction, user) => {
-  //logs when a reaction gets removed
-  if (reaction.message.partial) await reaction.message.fetch();
-  if (reaction.partial) await reaction.fetch();
-  if (user.bot) return;
-  if (!reaction.message.guild) return;
-  //REST OF THE CODE HERE
+  if (reaction.message.partial) await reaction.message.fetch(); // If it is a partial message, try to get as much info as possible.
+  if (reaction.partial) await reaction.fetch(); // If it is a partial message, try to get as much info as possible.
+  if (user.bot) return; // Do nothing if it is a bot reacting.
+  if (!reaction.message.guild) return; // Make sure the reaction is on a message within the server.
+
+  console.log('A reaction has been removed');
 };
