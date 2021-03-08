225cfff2fef0488654aebd0d2ca16ffab9b71654
Colenk <>
2021-03-08 16:43:30 -0500

    uncommented template so its easier to copy.

--- Util/template.js
+++ Util/template.js
@@ -1,17 +1,17 @@
-// module.exports = {
-//   //* Define the objects.
-//   name: 'commandname', // The name of the command.
-//   category: 'category', // The category the command will be listed at (for the help cmd).
-//   aliases: ['aliase'], // Array of aliases.
-//   args: true, // Whether the command requires arguments. Example: uptime and ping do not require arguments.
-//   guildOnly: false, // Whether the command can only be used in a guild. Example: You can run the ping command in a DM, as it doesn't require a server. You cannot run the kick command in a DM, as you cannot kick someone from a DM.
-//   usage: '<usage>', // An example of how to use the command. <> for required and [] for optional parameters.
-//   cooldown: 2, // Set the cooldown, in seconds.
-//   description: 'description', // The description of the command.
+module.exports = {
+  //* Define the objects.
+  name: 'commandname', // The name of the command.
+  category: 'category', // The category the command will be listed at (for the help cmd).
+  aliases: ['aliase'], // Array of aliases.
+  args: true, // Whether the command requires arguments. Example: uptime and ping do not require arguments.
+  guildOnly: false, // Whether the command can only be used in a guild. Example: You can run the ping command in a DM, as it doesn't require a server. You cannot run the kick command in a DM, as you cannot kick someone from a DM.
+  usage: '<usage>', // An example of how to use the command. <> for required and [] for optional parameters.
+  cooldown: 2, // Set the cooldown, in seconds.
+  description: 'description', // The description of the command.
 
   // A subfunction that runs the command with the following parameters: client, message, args, user, text (args.join(' ') from index.js), prefix.
-//   run: async (client, message, args, user, text, prefix) => {
+  run: async (client, message, args, user, text, prefix) => {
     // Everything in here can be a part of the command.
-//     message.reply('template command');
-//   }
-// };
+    message.reply('template command');
+  }
+};
