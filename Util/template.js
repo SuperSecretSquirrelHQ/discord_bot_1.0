8134a0ff9fb0ea4fd1d2830b0b29fe2638cddd38
Mrlilal <>
2021-03-08 00:06:02 -0500

    Updated the command template to reflect an actual command.

--- Util/template.js
+++ Util/template.js
@@ -1,13 +1,17 @@
 // module.exports = {
 //   //* Define the objects.
-//   name: 'commandname',
-//   category: 'category',
-//   aliases: ['aliase'],
-//   usage: '<usage>',
-//   cooldown: 2,
-//   description: 'description',
+//   name: 'commandname', // The name of the command.
+//   category: 'category', // The category the command will be listed at (for the help cmd).
+//   aliases: ['aliase'], // Array of aliases.
+//   args: true, 
+//   guildOnly: false, 
+//   usage: '<usage>', // Set the cooldown, in seconds.
+//   cooldown: 2, // An example of how to use the command. <> for required and [] for optional parameters.
+//   description: 'description', // The description of the command.
 
+  // A subfunction that runs the command with the following parameters: client, message, args, user, text (args.join(' ') from index.js), prefix.
 //   run: async (client, message, args, user, text, prefix) => {
+    // Everything in here can be a part of the command.
 //     message.reply('template command');
 //   }
 // };
