061dbbb9bc5acd55881868079e3ff0364aa3eee1
Colenk <>
2021-03-13 15:36:26 -0500

    Fixed typos.

--- commands/moderation/deleteMessage.js
+++ commands/moderation/deleteMessage.js
@@ -7,7 +7,7 @@ module.exports = {
     guildOnly: true, // Whether the command can only be used in a guild. Example: You can run the ping command in a DM, as it doesn't require a server. You cannot run the kick command in a DM, as you cannot kick someone from a DM.
     cooldown: 2, // Set the cooldown, in seconds.
     usage: 'deletemessage <Message ID> OR deletemessage <Number of messages>', // An example of how to use the command. <> for required and [] for optional parameters.
-    description: 'Delete one or many messages.', // The description of the command.
+    description: 'Delete a specific message, or several if the most recent messages.', // The description of the command.
 
     run: async (client, message, args, user, text, prefix) => {
         message.reply('template command');
